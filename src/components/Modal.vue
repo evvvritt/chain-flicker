c5149f0f6715b8a5ab1b7a6dd532d69eec8f5a6f
evvvritt <evv.info@gmail.com>
2017-12-11 13:01:20 +0100

    modal pad inside (ffx)

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -21,55 +21,55 @@ export default {
 </script>
 
 <style lang="scss">
-@import '../style/variables';
+  @import '../style/variables';
 
-.modal{
-  position: fixed;
-  padding:$frameWidth/8;
-  font-size:6vw;
+  .modal{
+    position: fixed;
+    padding:$frameWidth/8;
+    font-size:6vw;
 
 
-  > section{
-    position: relative;
-    border-radius:2.5rem;
-    padding: 4vmax;
-    @include screen($frameWidth);
-    top: auto; left:auto;
-    // scrolling
-    overflow-y:scroll;
-    -webkit-overflow-scrolling: touch;
-    &::-webkit-scrollbar { 
-      display: none; 
-    }
-    
-    .button{
-      position: absolute;
-      top:1rem; right:1rem;
-      z-index: 100;
-      &:after{
-        background-image:url('../assets/add-btn__plus.svg');
-        width:2rem;
-        height:2rem;
+    > section{
+      position: relative;
+      border-radius:2.5rem;
+      @include screen($frameWidth);
+      top: auto; left:auto;
+      // scrolling
+      overflow-y:scroll;
+      -webkit-overflow-scrolling: touch;
+      &::-webkit-scrollbar { 
+        display: none; 
+      }
+      
+      .button{
+        position: absolute;
+        top:1rem; right:1rem;
+        z-index: 100;
+        &:after{
+          background-image:url('../assets/add-btn__plus.svg');
+          width:2rem;
+          height:2rem;
+        }
+      }
+
+      > div{
+        max-width: 30em;
+        max-blend-mode:difference;
+        padding: 4vmax;
       }
     }
 
-    > div{
-      max-width: 30em;
-      max-blend-mode:difference;
+    p{
+      & + p{
+        margin-top: 1em;
+      }
     }
-  }
 
-  p{
-    & + p{
-      margin-top: 1em;
+    @media (orientation:portrait) {
+      > section{
+        @include screen($frameWidthPortrait);
+        top: auto; left:auto;
+      }
     }
   }
-
-  @media (orientation:portrait) {
-    > section{
-      @include screen($frameWidthPortrait);
-      top: auto; left:auto;
-    }
-  }
-}
 </style>
